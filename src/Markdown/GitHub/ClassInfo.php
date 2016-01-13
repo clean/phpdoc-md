17c9751dd6ae2c9b165718a7dd0fbd5133294f7c
Roman Nowicki <roman.nowicki@inbox.com>
2016-01-13 15:03:58 +0100

    added sortMethods config option...
    This option will affect how markdown output for class info will be
    formatted. If set to true all methods will be sorted alphabetically if
    set to false methods will be returned in original order
    
    return (object)[
        'rootNamespace' => 'Clean\PhpDocMd\Tests',
        'destDirectory' => 'docs',
        'format' => 'github',
        'sortMethods' => true,
        'classes' => [
            '\Clean\PhpDocMd\Tests\Example',
            '\Clean\PhpDocMd\Tests\Deep\Foo',
        ],
    ];

--- src/Markdown/GitHub/ClassInfo.php
+++ src/Markdown/GitHub/ClassInfo.php
@@ -7,21 +7,52 @@ use ReflectionClass;
 class ClassInfo extends Phtml
 {
     private $reflectionClass;
+    private $sortMethods = false;
 
+    /**
+     * __construct 
+     * 
+     * @param ReflectionClass $class class 
+     */
     public function __construct(ReflectionClass $class)
     {
         $this->setTemplate(__DIR__.'/../../../tpl/github.class.phtml');
         $this->reflectionClass = $class;
     }
 
+    /**
+     * When set to true methods will be sorted in markdown output
+     * 
+     * @param bool $value 
+     * 
+     * @return self
+     */
+    public function sortMethods($value)
+    {
+        assert(is_bool($value), 'Non bool parameter given for ' . __FUNCTION__);
+        $this->sortMethods = $value;
+        return $this;
+    }
+
+    /**
+     * Renders markdown for class
+     * 
+     * @return string
+     */
     public function render()
     {
         $parser = new ClassParser($this->reflectionClass);
+        $methods = $parser->getMethodsDetails();
+
+        if ($this->sortMethods) {
+            ksort($methods);
+        }
+
         $this->setData(
             [
                 'className' => $this->reflectionClass->getName(),
                 'classShortName' => $this->reflectionClass->getShortName(),
-                'methods' => $parser->getMethodsDetails(),
+                'methods' => $methods,
                 'classDescription' => $parser->getClassDescription(),
             ]
         );
